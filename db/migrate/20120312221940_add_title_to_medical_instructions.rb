b6711b20f8de561be3aa9fa9929aa370b5311d9f
Fattahul Alam <faruk@nascenia.com>
2012-03-13 04:47:45 +0600

    added plan title to medical instructions

---
+++ db/migrate/20120312221940_add_title_to_medical_instructions.rb
@@ -0,0 +1,17 @@
+class AddTitleToMedicalInstructions < ActiveRecord::Migration
+  def self.up
+    add_column(:medical_instructions, :title, :string)
+    add_column(:medical_instructions, :first_name, :string)
+    add_column(:medical_instructions, :last_name, :string)
+    add_column(:medical_instructions, :relation, :string)
+    add_column(:medical_instructions, :creator_id, :integer)
+  end
+
+  def self.down
+    remove_column(:medical_instructions, :title)
+    remove_column(:medical_instructions, :first_name)
+    remove_column(:medical_instructions, :last_name)
+    remove_column(:medical_instructions, :relation)
+    remove_column(:medical_instructions, :creator_id)
+  end
+end
