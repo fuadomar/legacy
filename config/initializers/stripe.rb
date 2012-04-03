143b59123cd74b5597d2680d5cb3317a22fa019a
nascenia <faruk@nascenia.com>
2012-04-03 20:38:02 +0600

    stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,6 +2,6 @@ if(Rails.env == 'development')
   Stripe.api_key = "lbTz9p7vv2Qw3FqCrQkIi8sSME7dLbA3"
   STRIPE_PUBLIC_KEY = "pk_HiQuX0vPapmGVtVI12DPykj6L2K6h"
 elsif(Rails.env == 'production')
-  Stripe.api_key = "lbTz9p7vv2Qw3FqCrQkIi8sSME7dLbA3"
-  STRIPE_PUBLIC_KEY = "pk_HiQuX0vPapmGVtVI12DPykj6L2K6h"
+  Stripe.api_key = "VG1wGCdH5WUpnkpeGPk9h1bFMfMkJCBG"
+  STRIPE_PUBLIC_KEY = "pk_MkI4oYnKhCty48op80kaqy1EHiGZ4"
 end
