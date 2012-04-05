ea2f773748afedf3fdb6baec282e9972e81b769e
nascenia <faruk@nascenia.com>
2012-04-05 13:05:54 +0600

    will and trust download link + session time out 6 hours added

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-NasceniaRailsTemplate::Application.config.session_store :cookie_store, :key => '_NasceniaRailsTemplate_session'
+NasceniaRailsTemplate::Application.config.session_store :cookie_store, :key => '_NasceniaRailsTemplate_session', :expire_after => 6.hours
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
