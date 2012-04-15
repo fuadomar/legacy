738f417a1d12828f16b9b1f195908e524c765082
nascenia <faruk@nascenia.com>
2012-04-15 18:17:39 +0600

    change default url optio0n to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 NasceniaRailsTemplate::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.action_mailer.default_url_options = { :host => 'ec2-23-21-1-121.compute-1.amazonaws.com' }
+  config.action_mailer.default_url_options = { :host => 'mylegacyplan.herokuapp.com' }
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
