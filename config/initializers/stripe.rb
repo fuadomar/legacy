ec8f0c3b96e151f8acc90035e85a21b9c4d78248
nascenia <faruk@nascenia.com>
2012-04-16 13:00:57 +0600

    change stripe test to live mode for production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,9 @@
 if(Rails.env == 'development')
-  Stripe.api_key = "lbTz9p7vv2Qw3FqCrQkIi8sSME7dLbA3"
-  STRIPE_PUBLIC_KEY = "pk_HiQuX0vPapmGVtVI12DPykj6L2K6h"
+#  Stripe.api_key = "lbTz9p7vv2Qw3FqCrQkIi8sSME7dLbA3"   #developer's test account
+#  STRIPE_PUBLIC_KEY = "pk_HiQuX0vPapmGVtVI12DPykj6L2K6h" #developer's test account
+  Stripe.api_key = "VG1wGCdH5WUpnkpeGPk9h1bFMfMkJCBG"     #Client's test account
+  STRIPE_PUBLIC_KEY = "pk_MkI4oYnKhCty48op80kaqy1EHiGZ4"  #Client's test account
 elsif(Rails.env == 'production')
-  Stripe.api_key = "VG1wGCdH5WUpnkpeGPk9h1bFMfMkJCBG"
-  STRIPE_PUBLIC_KEY = "pk_MkI4oYnKhCty48op80kaqy1EHiGZ4"
+  Stripe.api_key = "x4O6zTgUAI0DUho7HPtvRaCYX5LX0WFX"
+  STRIPE_PUBLIC_KEY = "pk_TGuqjL4D2oQMiq09LaBGpjF6MR9Hy"
 end
